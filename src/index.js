2198421f349befda7698e3bbfc4204ff67d7cc85
DinoCzar <ryanb_booking@yahoo.com>
2023-04-04 16:41:12 -0700

    Connect style.css to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('Hello Jupiter')
+
+const content = document.getElementById('content');
+
+const test = document.createElement('div');
+test.classList.add('test')
+test.textContent = 'Hello, Mars!';
+
+content.appendChild(test);
+
+
+

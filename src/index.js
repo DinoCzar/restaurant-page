e2a7a9cc8ae6b027824a410019f578705ecd4a72
DinoCzar <ryanb_booking@yahoo.com>
2023-04-06 01:36:01 -0700

    Add style.css skeleton, edit html elements

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-console.log('Hello Jupiter')
-
 const content = document.getElementById('content');
 
 const test = document.createElement('div');
-test.classList.add('test')
+test.classList.add('test');
 test.textContent = 'Hello, Mars!';
-
 content.appendChild(test);
 
-
-
+const yes = document.createElement('div');
+yes.classList.add('test');
+yes.textContent = 'Hello, Saturn!';
+content.appendChild(yes);

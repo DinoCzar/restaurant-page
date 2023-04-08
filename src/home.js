f57733a05acad08f029aff1adaabe1f5686e8373
DinoCzar <ryanb_booking@yahoo.com>
2023-04-07 22:11:14 -0700

    Add functions in home.js and index.js to remove page content when header buttons are clicked

--- src/home.js
+++ src/home.js
@@ -1,19 +1,28 @@
 function populateHome() {
+	const homePage = document.createElement('div');
+	homePage.setAttribute('id', 'home-page');
+	content.appendChild(homePage);
+
 	const restaurantName = document.createElement('div');
 	restaurantName.setAttribute('id', 'restaurant-name');
-	content.appendChild(restaurantName);
+	homePage.appendChild(restaurantName);
 	restaurantName.textContent = 'Java Spice';
 
 	const imageContainer = document.createElement('div');
 	imageContainer.setAttribute('id', 'image-container');
-	content.appendChild(imageContainer);
+	homePage.appendChild(imageContainer);
 	imageContainer.textContent = 'image here';
 
 	const description = document.createElement('div');
 	description.setAttribute('id', 'description');
-	content.appendChild(description);
+	homePage.appendChild(description);
 	description.textContent =
 		'We are an indonesian restuarant that has been in business since 2001. We pride ourselves in serving the most authentic and delicious creations from our home town, Denpasar, Bali. We always cook with love, and are excited to share some of our unique spices with you!';
+	return homePage;
 }
 
-export default populateHome;
+function loadHome() {
+    return populateHome();
+} 
+
+export default loadHome;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,57 @@
-import populateHeader from './header';
-import populateHome from './home';
+import { remove } from 'lodash';
+import loadHome from './home';
+
+function populateHeader() {
+
+    const loadHomeTest = loadHome();
+	const content = document.getElementById('content');
+
+	const header = document.createElement('div');
+	header.setAttribute('id', 'header');
+	content.appendChild(header);
+
+	const homeDiv = document.createElement('div');
+	homeDiv.setAttribute('id', 'home-div');
+	header.appendChild(homeDiv);
+
+	const menuDiv = document.createElement('div');
+	menuDiv.setAttribute('id', 'menu-div');
+	header.appendChild(menuDiv);
+
+	const contactDiv = document.createElement('div');
+	contactDiv.setAttribute('id', 'contact-div');
+	header.appendChild(contactDiv);
+
+	const home = document.createElement('button');
+	home.setAttribute('id', 'home');
+	home.classList.add('button');
+	homeDiv.appendChild(home);
+	home.textContent = 'Home';
+	home.addEventListener('click', (e) => {
+		console.log(homePageDiv);
+	});
+
+	const menu = document.createElement('button');
+	menu.setAttribute('id', 'menu');
+	menu.classList.add('button');
+	menuDiv.appendChild(menu);
+	menu.textContent = 'Menu';
+	menu.addEventListener('click', (e) => {
+		loadHomeTest.remove();
+	});
+
+	const contact = document.createElement('button');
+	contact.setAttribute('id', 'contact');
+	contact.classList.add('button');
+	contactDiv.appendChild(contact);
+	contact.textContent = 'Contact';
+	contact.addEventListener('click', (e) => {
+		description.remove();
+	});
+}
 
 function populateWebsite() {
 	populateHeader();
-	populateHome();
 }
 
 populateWebsite();

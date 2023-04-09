dc88d2c6a92b89ce80260413087c6b0463060f01
DinoCzar <ryanb_booking@yahoo.com>
2023-04-08 22:31:53 -0700

    Add styling for menu page

--- src/menu.js
+++ src/menu.js
@@ -22,18 +22,36 @@ function populateMenu() {
 		return { item, details, generateMenuItem };
 	};
 
-	const nasigoreng = menuFactory(
+	const menuItem1 = menuFactory(
 		'Nasi Goreng',
-		'delicious fried rice with egg'
+		'fried rice dish made with steamed rice stir-fried with kecap manis, sambal, shrimp paste, garlic, onion, vegetables, and chicken'
 	);
 
-	const friedrice = menuFactory(
-		'Fried rice',
-		'yummy rice'
+	const menuItem2 = menuFactory(
+		'Nasi Campur',
+		'grilled chicken, spicy beef rendang, fried tempeh, stir-fried kangkung, sambal, and a refreshing salad'
 	);
 
-	nasigoreng.generateMenuItem();
-	friedrice.generateMenuItem();
+	const menuItem3 = menuFactory(
+		'Pempek Telor',
+		'fried fish cakes made from a mixture of fish and tapioca starch, stuffed with a boiled egg, and served with a tangy vinegar-based dipping sauce and sprinkled with chopped cucumber and shallots'
+	);
+
+	const menuItem4 = menuFactory(
+		'Sayur Lodeh',
+		'coconut milk-based vegetable stew featuring a medley of fresh vegetables such as eggplant, green beans, and cabbage, simmered in a fragrant broth of lemongrass, galangal, and turmeric, and served with steamed rice'
+	);
+
+	const menuItem5 = menuFactory(
+		'Mie Ayam Bakso',
+		'egg noodles served with slices of tender chicken, meatballs, crispy fried shallots, and scallions, all in a savory chicken broth'
+	);
+
+	menuItem1.generateMenuItem();
+	menuItem2.generateMenuItem();
+	menuItem3.generateMenuItem();
+	menuItem4.generateMenuItem();
+	menuItem5.generateMenuItem();
 
 	return menuPage;
 }

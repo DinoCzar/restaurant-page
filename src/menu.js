b2db791dd61ff1f78b6a560d756e15a3ff5f7dc7
DinoCzar <ryanb_booking@yahoo.com>
2023-04-08 17:41:53 -0700

    Add menu item creation function to menu.js, add menu item styling to style.css

--- src/menu.js
+++ src/menu.js
@@ -2,10 +2,38 @@ function populateMenu() {
 	const menuPage = document.createElement('div');
 	menuPage.setAttribute('id', 'menu-page');
 
-	const menuItem = document.createElement('div');
-	menuItem.setAttribute('id', 'menu-item');
-	menuPage.appendChild(menuItem);
-	menuItem.textContent = 'menu item';
+	const menuFactory = (item, details) => {
+		const generateMenuItem = () => {
+			const menuItem = document.createElement('div');
+			menuItem.setAttribute('id', 'menu-item');
+			menuPage.appendChild(menuItem);
+
+			const itemTitle = document.createElement('div');
+			itemTitle.setAttribute('id', 'item-title');
+			menuItem.appendChild(itemTitle);
+			itemTitle.textContent = item;
+
+			const itemDetails = document.createElement('div');
+			itemDetails.setAttribute('id', 'item-details');
+			menuItem.appendChild(itemDetails);
+			itemDetails.textContent = details;
+		};
+
+		return { item, details, generateMenuItem };
+	};
+
+	const nasigoreng = menuFactory(
+		'Nasi Goreng',
+		'delicious fried rice with egg'
+	);
+
+	const friedrice = menuFactory(
+		'Fried rice',
+		'yummy rice'
+	);
+
+	nasigoreng.generateMenuItem();
+	friedrice.generateMenuItem();
 
 	return menuPage;
 }

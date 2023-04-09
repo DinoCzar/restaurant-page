2f4e0a65df2f73ed932047a0219ff381936dc822
DinoCzar <ryanb_booking@yahoo.com>
2023-04-08 23:00:14 -0700

    Add styling, functions, and content to contact.js

--- src/contact.js
+++ src/contact.js
@@ -1,11 +1,34 @@
 function populateContact() {
 	const contactPage = document.createElement('div');
-	contactPage.setAttribute('id', 'contact-page');
+	contactPage.setAttribute('id', 'home-page');
+
+	const restaurantName = document.createElement('div');
+	restaurantName.setAttribute('id', 'restaurant-name');
+	contactPage.appendChild(restaurantName);
+	restaurantName.textContent = 'Java Spice';
+
+	const imageContainer = document.createElement('div');
+	imageContainer.setAttribute('id', 'image-container');
+	contactPage.appendChild(imageContainer);
+
+	const homeImage = document.createElement('img');
+	homeImage.src = 'nasi-goreng.jpg';
+	homeImage.setAttribute('id', 'home-image');
+	imageContainer.appendChild(homeImage);
 
 	const contactInfo = document.createElement('div');
 	contactInfo.setAttribute('id', 'contact-info');
 	contactPage.appendChild(contactInfo);
-	contactInfo.textContent = 'contact info';
+
+	const address = document.createElement('div');
+	address.setAttribute('id', 'address');
+	contactInfo.appendChild(address);
+	address.textContent = '1234 Delicious Ln Irvine CA 92618';
+
+	const phone = document.createElement('div');
+	phone.setAttribute('id', 'phone');
+	contactInfo.appendChild(phone);
+	phone.textContent = '(123)456-7890';
 
 	return contactPage;
 }

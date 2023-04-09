b210e8a239dec9b3ae6930d2de7410e294e807e6
DinoCzar <ryanb_booking@yahoo.com>
2023-04-08 22:44:55 -0700

    Update menu page spacing

--- src/menu.js
+++ src/menu.js
@@ -24,27 +24,27 @@ function populateMenu() {
 
 	const menuItem1 = menuFactory(
 		'Nasi Goreng',
-		'fried rice dish made with steamed rice stir-fried with kecap manis, sambal, shrimp paste, garlic, onion, vegetables, and chicken'
+		'Fried rice dish made with steamed rice stir-fried with kecap manis, sambal, shrimp paste, garlic, onion, vegetables, and chicken'
 	);
 
 	const menuItem2 = menuFactory(
 		'Nasi Campur',
-		'grilled chicken, spicy beef rendang, fried tempeh, stir-fried kangkung, sambal, and a refreshing salad'
+		'Grilled chicken, spicy beef rendang, fried tempeh, stir-fried kangkung, sambal, and a refreshing salad'
 	);
 
 	const menuItem3 = menuFactory(
 		'Pempek Telor',
-		'fried fish cakes made from a mixture of fish and tapioca starch, stuffed with a boiled egg, and served with a tangy vinegar-based dipping sauce and sprinkled with chopped cucumber and shallots'
+		'Fried fish cakes made from a mixture of fish and tapioca starch, stuffed with a boiled egg, and served with a tangy vinegar-based dipping sauce and sprinkled with chopped cucumber and shallots'
 	);
 
 	const menuItem4 = menuFactory(
 		'Sayur Lodeh',
-		'coconut milk-based vegetable stew featuring a medley of fresh vegetables such as eggplant, green beans, and cabbage, simmered in a fragrant broth of lemongrass, galangal, and turmeric, and served with steamed rice'
+		'Coconut milk-based vegetable stew featuring a medley of fresh vegetables such as eggplant, green beans, and cabbage, simmered in a fragrant broth of lemongrass, galangal, and turmeric, and served with steamed rice'
 	);
 
 	const menuItem5 = menuFactory(
 		'Mie Ayam Bakso',
-		'egg noodles served with slices of tender chicken, meatballs, crispy fried shallots, and scallions, all in a savory chicken broth'
+		'Egg noodles served with slices of tender chicken, meatballs, crispy fried shallots, and scallions, all in a savory chicken broth'
 	);
 
 	menuItem1.generateMenuItem();
